8e931ad069d46df85647c54107ca38670888e073
Muhammad Muhajir <muhajirframe@gmail.com>
2019-03-10 10:36:46 +0700

    Fix(layout): screen was scrollabel to the bottom

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,11 @@
 import { Flex } from "rebass"
 import styled from "styled-components"
-import { maxWidth, minWidth, minHeight, maxHeight } from "styled-system"
+import { maxWidth, minWidth, minHeight, maxHeight, height } from "styled-system"
 
 export default styled(Flex)`
   ${maxWidth};
   ${minWidth};
   ${minHeight};
   ${maxHeight};
+  ${height}
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import { Flex, Box, Text } from "rebass"
+import { Flex, Box, Text, Image } from "rebass"
 import Container from "../components/Container"
 import Input from "../components/Input"
 import Button from "../components/button"
@@ -14,27 +13,34 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
 
-    <Flex
-      css={{ position: "absolute" }}
-      width={1}
-      pr={5}
-      pt={4}
-      justifyContent="flex-end"
-    >
-      <Text fontSize={3} color="subheading" fontWeight="bold">
-        Signup →
-      </Text>
-    </Flex>
-    <Box width={1} css={{ position: "absolute", bottom: -120 }}>
-      <img src={wave} width="100%" />
-    </Box>
     <Container
       bg="background"
-      minHeight="100vh"
+      height="100vh"
       justifyContent="center"
       flexDirection="column"
+      css={{ overflowY: "hidden" }}
     >
-      <Flex flexDirection="column" alignItems="stretch" width="25rem" mx="auto">
+      <Flex
+        css={{ position: "absolute", top: 0 }}
+        width={1}
+        pr={5}
+        pt={4}
+        justifyContent="flex-end"
+      >
+        <Text fontSize={3} color="subheading" fontWeight="bold">
+          Signup →
+        </Text>
+      </Flex>
+      <Box width={1} css={{ position: "absolute", bottom: 0 }}>
+        <Image src={wave} width="100%" />
+      </Box>
+      <Flex
+        flexDirection="column"
+        alignItems="stretch"
+        width="25rem"
+        mx="auto"
+        height="100%"
+      >
         <Box>
           <Text fontSize={3} color="white" fontSize={5} fontWeight="500" mb={3}>
             Here you can login
